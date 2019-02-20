9a634ba4dbee732d43d725c9edeb22c658327fc5
Jesse Hang <hangtimee@gmail.com>
2019-02-20 15:20:14 -0800

    Implemented stack.js and queue.js for functional shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
-var Stack = function() {
+var Stack = function(obj) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
+
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  var size = Object.keys(this.storage).length;
+  this.storage[size] = value;
+};
 
+stackMethods.size = function () {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
+
+stackMethods.pop = function() {
+  var keys = Object.keys(this.storage);
+  //[1,2,3,4]
+  var size = Object.keys(this.storage).length;
+  var keep = this.storage[keys[size - 1]];
+  delete this.storage[keys[size - 1]];
+  return keep;
+};
