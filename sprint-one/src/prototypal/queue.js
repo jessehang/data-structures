e7e725eec4bff5e30dfae4657633be6e3ad20ac4
Jesse Hang <hangtimee@gmail.com>
2019-02-20 15:41:07 -0800

    Implemeneted stack.js and queue.js methods for the prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (value) {
+  var number = Object.keys(this.storage).length;
+  if (this.storage[number]) {
+    number += 1;
+    this.storage[number] = value;
+  } else {
+    this.storage[number] = value;
+  }
+};
+
+queueMethods.size = function () {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
+
+queueMethods.dequeue = function () {
+  var keys = Object.keys(this.storage);
+  var keep = this.storage[keys[0]];
+  delete this.storage[keys[0]];
+  return keep;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  var size = Object.keys(this.storage).length;
+  this.storage[size] = value;
+};
 
+stackMethods.size = function () {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
+
+stackMethods.pop = function () {
+  var keys = Object.keys(this.storage);
+  //[1,2,3,4]
+  var size = Object.keys(this.storage).length;
+  var keep = this.storage[keys[size - 1]];
+  delete this.storage[keys[size - 1]];
+  return keep;
+};
