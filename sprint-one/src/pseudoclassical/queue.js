3e59e3005a8d7828ec9ec7b377d18b8eb1d5f617
Jesse Hang <hangtimee@gmail.com>
2019-02-20 15:46:10 -0800

    Implemented the pseudoclassical pattern for stack.js and queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function (value) {
+  var number = Object.keys(this.storage).length;
+  if (this.storage[number]) {
+    number += 1;
+    this.storage[number] = value;
+  } else {
+    this.storage[number] = value;
+  }
+};
+
+Queue.prototype.size = function () {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
+
+Queue.prototype.dequeue = function () {
+  var keys = Object.keys(this.storage);
+  var keep = this.storage[keys[0]];
+  delete this.storage[keys[0]];
+  return keep;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Stack.prototype.push = function (value) {
+  var size = Object.keys(this.storage).length;
+  this.storage[size] = value;
+};
+
+Stack.prototype.size = function () {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
+
+Stack.prototype.pop = function () {
+  var keys = Object.keys(this.storage);
+  //[1,2,3,4]
+  var size = Object.keys(this.storage).length;
+  var keep = this.storage[keys[size - 1]];
+  delete this.storage[keys[size - 1]];
+  return keep;
 };
 
 
