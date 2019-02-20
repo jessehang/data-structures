05e1bc891a971050c6f9c16de6ea56d835933c7f
Jesse Hang <hangtimee@gmail.com>
2019-02-20 15:52:48 -0800

    Implemented es6 pattern for stack.js and queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var number = Object.keys(this.storage).length;
+    if (this.storage[number]) {
+      number += 1;
+      this.storage[number] = value;
+    } else {
+      this.storage[number] = value;
+    }
+  }
+
+  size() {
+    var size = Object.keys(this.storage).length;
+    return size;
+  }
+
+  dequeue() {
+    var keys = Object.keys(this.storage);
+    var keep = this.storage[keys[0]];
+    delete this.storage[keys[0]];
+    return keep;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var size = Object.keys(this.storage).length;
+    this.storage[size] = value;
+  }
+
+  size() {
+    var size = Object.keys(this.storage).length;
+    return size;
+  }
+
+  pop() {
+    var keys = Object.keys(this.storage);
+    var size = Object.keys(this.storage).length;
+    var keep = this.storage[keys[size - 1]];
+    delete this.storage[keys[size - 1]];
+    return keep;
   }
 
 }
