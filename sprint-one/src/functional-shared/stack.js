badfbb2d2ef9fad7d1e77d7cb2d325788f13d56e
Jesse Hang <hangtimee@gmail.com>
2019-02-20 15:31:03 -0800

    Updated completed version of stack.js for functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,4 +1,4 @@
-var Stack = function(obj) {
+var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {
@@ -31,7 +31,6 @@ stackMethods.size = function () {
 
 stackMethods.pop = function() {
   var keys = Object.keys(this.storage);
-  //[1,2,3,4]
   var size = Object.keys(this.storage).length;
   var keep = this.storage[keys[size - 1]];
   delete this.storage[keys[size - 1]];
