3f9c7eb4e5f1d09c88cdce202450100d073f3356
jessehang <hangtimee@gmail.com>
2019-02-20 19:28:10 -0800

    Implemented LinkedList methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.head !== null) {
+      var newTail = Node(value);
+      list.tail.next = newTail;
+      list.tail = newTail;
+    } else {
+      list.head = Node(value);
+      list.tail = list.head;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var formerHead = list.head.value;
+      list.head = list.head.next;
+      return formerHead;
+    }
   };
 
   list.contains = function(target) {
+    while(list.head !== null) {
+      if (list.head.value === target) {
+        return true;
+      } 
+      list.head = list.head.next;
+    }
+    return false;
   };
 
   return list;
 };
+
+
 
 var Node = function(value) {
   var node = {};
