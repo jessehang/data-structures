d2f79757e4257797c559a9f517bff828ca1e5081
Jesse Hang <hangtimee@gmail.com>
2019-02-22 10:33:56 -0800

    Implemented hashtable.js methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,4 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +6,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index)) {
+    this._storage.get(index)[k] = v;//Check if this accesses storage and sets new key
+  } else {
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(index, obj);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index)[k] = undefined;
+  //this._storage.set(index, this._storage.get(index)[key]undefined);
 };
 
 
