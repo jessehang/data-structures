b5f4775f2c9d5c13107f27dce9553906399fa847
Jesse Hang <hangtimee@gmail.com>
2019-02-21 17:19:34 -0800

    Implemented set methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { //constant time complexity
+  if (!this.contains(item)) {
+    this._storage[item] = item;
+  }
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { //linear time complexity
+  return this._storage.hasOwnProperty(item);
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { //linear time complexity
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      delete this._storage[key];
+    }
+  }
 };
 
 /*
